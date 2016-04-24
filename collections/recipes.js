e8e0fa2defee8116e89d201f3984cc23119c5a39
Alfonso Diaz <alfonso.dzalo@gmail.com>
2016-04-24 10:27:07 -0700

    Sub-documents & Array Fields

--- collections/recipes.js
+++ collections/recipes.js
@@ -2,29 +2,51 @@
 Recipes = new Mongo.Collection('recipes');
 
 Recipes.allow({
-  // If user ID excits, it means you are signed in and you are able to add a recipe
-  //this is necessary since we deleted predefined packges from the packages file
+  // If user ID excits, it means you are signed in and you are able to add a recipe.
+  //this is necessary since we deleted predefined packges from the packages file.
   insert: function(userId, doc){
   return !!userId;
   }
 });
 
+Ingredient = new SimpleSchema({
+//
+  name: {
+    type: String
+  },
+  amount: {
+    type: String
+  }
+});
+
 RecipeSchema = new SimpleSchema({
-// Inside this object we fill in the properties that we want our schema to have
+// Inside this object we fill in the properties that we want our schema to have.
 //We can tell which field is required or not. By defaulst all fields are required.
   name: {
-    //This is another obeject and here we define its properties
+    //This is another obeject and here we define its properties.
     type: String,
-    label: "Name" //Label used in the formfield
+    label: "Name" //Label used in the formfield.
   },
   desc: {
     type: String,
     label: "Description"
   },
+  ingredients: {
+    type: [Ingredient] //By puting it in [] it became an array and meteor allows us to have incrementing box fields. Otherwise it'd show as the name field above (single box field).
+  },
+  inMenu: {
+    //this property will check if the recipi is in this week's menu and if so, it will add the ingredients to the shopping list.
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
+        type: "hidden" //We don't want the user to see this.
+    }
+  },
   author: {
     type: String,
     label: "Author",
-    autoValue: function(){ //This will give it a default value
+    autoValue: function(){ //This will give it a default value.
       return this.userId
     },
     autoform: {
