219443c28622c7e2d0a9ec940a6ae9ff54986fe7
Alfonso Diaz <alfonso.dzalo@gmail.com>
2016-04-24 11:04:52 -0700

    Private & Public Settings and Google Analytics

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,1 +1,7 @@
+/*Anything added should not be visible in your code and not in your repo*/
+/*since you might store very sensitve infor such a s private keys*/
+/*This is for google analytics pacakge called 'ga'.*/
+/*When doing google analytics you will be give a number which you will add below in account*/
+
 Meteor.subscribe('recipes');
+console.log(Meteor.settings.public.ga.account);
