2086170f8aa24573d0f49ae0998c82575772ab4c
Alfonso Diaz <alfonso.dzalo@gmail.com>
2016-04-26 22:10:48 -0700

    Single Item Subscriptions

--- server/publish.js
+++ server/publish.js
@@ -2,3 +2,8 @@ Meteor.publish('recipes', function(){
   return Recipes.find({author: this.userId});
   // Only the current user's recipes will be available to them
 });
+
+Meteor.publish('singleRecipe', function(id){
+  check(id, String);
+  return Recipes.find({_id: id});
+});
