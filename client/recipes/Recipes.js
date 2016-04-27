501b075630d249d85191cebf992d87fa1fdfdf41
Alfonso Diaz <alfonso.dzalo@gmail.com>
2016-04-26 21:38:10 -0700

    Template Level Subscriptions

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -3,7 +3,18 @@
 /*This is for google analytics pacakge called 'ga'.*/
 /*When doing google analytics you will be give a number which you will add below in account*/
 
-Meteor.subscribe('recipes');
+// subscription managment is important for a performing application
+//subscriptions to recipes will show the recipes of ALL users signed in. Not very practiva so we'll change that
+// Meteor.subscribe('recipes');
+// To optmize we will use template lvl subscriptions. Once the template is loaded, then we are going to subscribe to the correct data.
+
+Template.Recipes.onCreated(function(){
+  var self = this;
+  // The selfautorun will unsubscribe us from old subscriptions
+  self.autorun(function() {
+    self.subscribe('recipes');
+  });
+});
 
 /* curly braquests {} are called 'big event signs'*/
 Template.Recipes.helpers({
